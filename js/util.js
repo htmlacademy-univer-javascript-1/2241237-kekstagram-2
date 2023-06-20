6de1eaf322e2ec053c52fe47f4fb0c42e46197d5
Igor Stolbov <knigar.igor@yandex.ru>
2023-06-21 00:37:48 +0500

    12.11

--- js/util.js
+++ js/util.js
@@ -2,9 +2,31 @@ function generateRandom(a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = Math.random() * (upper - lower + 1) + lower;
+
   return Math.floor(result);
 }
-function checkLength(verifiedString, maxLength) {
-  return (verifiedString.length >= maxLength);
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
 }
-export {generateRandom, checkLength};
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export { generateRandom, debounce, throttle };
